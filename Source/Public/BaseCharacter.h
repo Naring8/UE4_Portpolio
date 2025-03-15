045c17f91d60bc00974a6d046d3809e0babfb06c
Naring8 <skell01@naver.com>
2025-03-15 20:04:16 +0900

    Edit Animation-related Function
    use 'const' to remain variable steady

--- Source/Public/BaseCharacter.h
+++ Source/Public/BaseCharacter.h
@@ -5,10 +5,11 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include <../Interfaces/CharacterMovementInterface.h>
+#include <../Interfaces/AnimationInterface.h>
 #include "BaseCharacter.generated.h"
 
 UCLASS()
-class UE4_PORTPOLIO_API ABaseCharacter : public ACharacter, public ICharacterMovementInterface
+class UE4_PORTPOLIO_API ABaseCharacter : public ACharacter, public ICharacterMovementInterface, public IAnimationInterface
 {
 	GENERATED_BODY()
 
@@ -16,6 +17,7 @@ public:
 	ABaseCharacter();
 
 	virtual void OnConstruction(FTransform const& Transform) override;
+
 protected:
 	virtual void BeginPlay() override;
 
@@ -25,11 +27,15 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual void PlayAnimation(class UAnimMontage* const Montage, float const PlayRate, FName const Section) override;
+
+	void ResetToIdle(UAnimMontage* const Montage, bool const bInterrupted);
+
 private:
 	UFUNCTION(BlueprintCallable)
 		void CustomCrouch();
 	UFUNCTION(BlueprintCallable)
-		void CustomUnCrouch();
+		void CustomUncrouch();
 
 private:
 	virtual void Walk() override;
@@ -52,5 +58,4 @@ private:
 	float DefaultCapsuleRadius = 35.0f;
 
 	float CrouchCapsuleHalfHeight = 65.0f;
-
 };
